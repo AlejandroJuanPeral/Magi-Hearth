060df344b10e943852e95463a1626e40ed5b6624
axel8890 <axel8890@gmail.com>
2018-12-19 14:13:51 +0100

    puzle y peurta acabados

--- Assets/Scripts/DoorPuzzle.cs
+++ Assets/Scripts/DoorPuzzle.cs
@@ -1,26 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorPuzzle : MonoBehaviour
 {
     int numElement;
     public GameObject[] Parts;
+    public Sprite[] Runes;
+    public GameObject RuneSprite, Door;
     string[,] Puzzles;
-    int numPuzzle;
+    public int numPuzzle;
     
     // Start is called before the first frame update
     void Start()
     {
         numElement = 1;
-        numPuzzle = 0;
-        Puzzles = new string[1, 6];
-        Puzzles[0, 0] = "Patas";
+        numPuzzle = Random.Range(0,4);
+        RuneSprite.GetComponent<Image>().sprite = Runes[numPuzzle];
+        Puzzles = new string[4, 6];
+        Puzzles[0, 0] = "Cola";
         Puzzles[0, 1] = "Garras";
         Puzzles[0, 2] = "Cabeza";
         Puzzles[0, 3] = "Alas";
-        Puzzles[0, 4] = "Cola";
+        Puzzles[0, 4] = "Patas";
         Puzzles[0, 5] = "Torso";
+
+        Puzzles[1, 0] = "Alas";
+        Puzzles[1, 1] = "Torso";
+        Puzzles[1, 2] = "Cabeza";
+        Puzzles[1, 3] = "Cola";
+        Puzzles[1, 4] = "Patas";
+        Puzzles[1, 5] = "Garras";
+
+        Puzzles[2, 0] = "Torso";
+        Puzzles[2, 1] = "Cabeza";
+        Puzzles[2, 2] = "Alas";
+        Puzzles[2, 3] = "Garras";
+        Puzzles[2, 4] = "Patas";
+        Puzzles[2, 5] = "Cola";
+
+        Puzzles[3, 0] = "Cabeza";
+        Puzzles[3, 1] = "Cola";
+        Puzzles[3, 2] = "Garras";
+        Puzzles[3, 3] = "Alas";
+        Puzzles[3, 4] = "Torso";
+        Puzzles[3, 5] = "Patas";
 
     }
 
@@ -42,7 +67,8 @@ public class DoorPuzzle : MonoBehaviour
         {
             if (Comprobar(NewPart))
             {
-                //Open door
+                Debug.Log("Opendoor");
+                Door.GetComponent<OpenDoor>().StartOpenDoor();
             }
             else
             {
---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public bool open = false;
+    float startTime, currentTime;
+    Vector3 now, end; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        end = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+
+    }
+    public void StartOpenDoor()
+    {
+        open = true;
+        startTime = Time.time;
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (open)
+        {
+            now = transform.position;
+            currentTime += Time.deltaTime;
+            transform.position = Vector3.Slerp(now, end, (currentTime - startTime)/6);
+        }
+ 
+    }
+}
